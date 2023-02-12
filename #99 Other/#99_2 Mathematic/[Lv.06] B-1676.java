0204ab7322a2301add10a6ead3693512e95f0868
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-13 07:13:26 +0900

    [BaekJoon] [S5] 팩토리얼 0의 개수
    팩토리얼 수의 맨 뒤부터, 0의 개수가 몇개인지 세서 출력하는 문제.
    
    ex) 10! 는 3628800이고, 맨 뒤부터 2개의 숫자가 0이므로 2를 출력해야 한다.
    
    최대 500! 까지 연산을 해야하는 문제이기 때문에 큰 수를 계산할 수 있는 BigInteger 클래스를 선언하여 사용,
    팩토리얼 수 계산 후 문자열로 바꿔주고, 맨 뒤부터 차례로 체크했다.
    0을 만날때마다 count 변수에 1을 더해주고, 마지막에 count 변수를 출력하는 방식으로 풀었다.

---
+++ #99 Other/#99_2 Mathematic/[Lv.06] B-1676.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger fac = BigInteger.ONE;
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            fac = fac.multiply(BigInteger.valueOf(i));
+        }
+      
+        String str = fac.toString();
+        int count = 0;
+        for (int i = str.length()-1; i >= 0; i--) {
+            if(str.charAt(i) == '0') count++;
+            else break;
+        }
+      
+        System.out.println(count);
+    }
+}
