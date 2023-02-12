b0cd2da2704f903430336b54d0e3206cd3bb3a51
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-13 05:44:16 +0900

    [BaekJoon] [B1] 최대공약수와 최소공배수
    [최대공약수 = (두 수의 곱) / 최소공배수] 공식을 이용.

---
+++ #99 Other/#99_2 Mathematic/[Lv.05] B-2609.java
@@ -0,0 +1,24 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int L = 0;
+
+        for (int i = 1; i <= Math.max(N,M); i++)
+            if(N%i == 0 && M%i == 0) L = i;
+
+        int G = (N*M/L);
+
+        System.out.println(L);
+        System.out.println(G);
+
+    }
+}
