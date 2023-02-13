ee6e0bcf281b0c6131d4da73873cf61c0707a2c9
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-13 10:59:58 +0900

    [BaekJoon] [S2] 잃어버린 괄호
    + 와 -, 숫자를 이용해 만들어진 식에 괄호를 추가해 식의 값을 최소로 만들어 출력하는 문제.
    
    - 가 하나 있으면 그 뒤에 있는 수들은 모두 음수로 만들 수 있다.
    Parsing 부분이 중요한 문제.

---
+++ #4 Problem Solving Strategy/#4_3 Greedy Algorithm/[Lv.09] B-1541.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> arr = new ArrayList<>();
+
+        String str = rd.readLine();
+
+        for (int i = 0; i <= str.length() - 1; i++) {
+            if (str.charAt(i) == '+' || str.charAt(i) == '-') {
+                arr.add(sb.toString());
+                arr.add(Character.toString(str.charAt(i)));
+                sb = new StringBuilder();
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        arr.add(sb.toString());
+
+        int N = Integer.parseInt(arr.get(0));
+        for (int i = 1; i < arr.size(); i += 2) {
+            if (arr.get(i).equals("+"))
+                N += Integer.parseInt(arr.get(i + 1));
+            else {
+                for (int j = i + 1; j < arr.size(); j += 2)
+                    N -= Integer.parseInt(arr.get(j));
+                break;
+            }
+        }
+        System.out.println(N);
+    }
+}
+
+
+// 20 + 90 - 30 + 50 - 30 + 10 - 20 + 50 + 90
+// 마이너스가 하나 있으면 그 뒤로는 마이너스로 무조건 만들 수 있다.
