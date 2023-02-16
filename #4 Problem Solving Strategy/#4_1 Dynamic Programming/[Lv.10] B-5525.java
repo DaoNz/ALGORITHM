90cf5da1b1e3e3be11ce67465efc4d6eb51362cb
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-17 02:42:31 +0900

    [BaekJoon] [S1] IOIOI
    정수형으로 N과 M이 주어지고, 'I'와 'O'로 구성된 문자열 S가 주어진다.
    문자열 S에 N의 값에 따른 조건을 가진 문자열이 몇개나 포함되어 있는 지 계산하여 출력하는 문제.
    
    N이 1일 때 조건은 "IOI"가 되고, 1씩 늘어날수록 뒤에 OI가 추가로 붙는다.
    ex) N이 1이고 S가 OOIOIOIOIIOII 라면. S 안에 "IOI"가 4개 포함되어 있으므로 답은 4가 된다.
    
    DP 방식으로 풀었다. 두번째 문자열부터 charAt 메소드를 사용하며 확인한다.
    현재 문자가 'I'이면서 이전문자가 'I' 이거나, 현재 문자가 'I'이면서 두번째 전 문자가 'O'라면 DP 배열에 1을 저장한다.
    두번째 전 문자부터 현재 문자까지 "IOI"가 성립된다면, 두번째 전 DP배열의 값 + 1을 DP배열에 저장하고,
    마지막에 N보다 큰 값의 개수를 세 출력한다.
    
    생각해보니 substring으로 3개 문자를 가져와 equals("IOI")를 사용했으면
    IF문을 더 적게 사용하여 풀이할 수 있지 않았을까?

---
+++ #4 Problem Solving Strategy/#4_1 Dynamic Programming/[Lv.10] B-5525.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(rd.readLine());
+        int M = Integer.parseInt(rd.readLine());
+        String data = rd.readLine();
+
+        int[] dp = new int[M];
+        int res = 0;
+
+        if (data.charAt(0) == 'I') dp[0] = 1;
+        if (data.charAt(1) == 'I') dp[1] = 1;
+
+        for (int i = 2; i < M; i++) {
+            if (data.charAt(i - 1) == 'I' && data.charAt(i) == 'I') dp[i] = 1;
+            else if (data.charAt(i - 2) == 'O' && data.charAt(i) == 'I') dp[i] = 1;
+            else if (data.charAt(i - 2) == 'I' && data.charAt(i) == 'I') dp[i] = dp[i - 2] + 1;
+            if (dp[i] > N) res++;
+        }
+
+//        System.out.println(data);
+//        for (int i : dp) System.out.print(i);
+//        System.out.println();
+        System.out.print(res);
+    }
+}
