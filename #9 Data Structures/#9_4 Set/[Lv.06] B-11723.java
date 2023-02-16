f3e6802be46049d8530c12e1fc5c9d6ca6931167
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-17 05:03:20 +0900

    [BaekJoon] [S5] 집합
    주어진 문자열에 따라 Set을 이용하여 연산을 수행하고 check 결과값을 연산 결과값을 출력하는 문제.
    
    Set보다는 Switch Case 복습이랑, 잘 안쓰게되던 List 클래스 사용법을 다시 익히는데 좀 더 도움이 된듯.

---
+++ #9 Data Structures/#9_4 Set/[Lv.06] B-11723.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        HashSet<Integer> set = new HashSet<>();
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+        int N = Integer.parseInt(rd.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer token = new StringTokenizer(rd.readLine(), " ");
+            String method = token.nextToken();
+            int x = 0;
+
+            if (token.hasMoreTokens()) x = Integer.parseInt(token.nextToken());
+
+            switch (method) {
+                case "add":
+                    set.add(x);
+                    break;
+
+                case "remove":
+                    set.remove(x);
+                    break;
+
+                case "check":
+                    if (set.contains(x)) sb.append(1).append("\n");
+                    else sb.append(0).append("\n");
+                    break;
+
+                case "toggle":
+                    if (set.contains(x)) set.remove(x);
+                    else set.add(x);
+                    break;
+
+                case "all":
+                    set.addAll(list);
+                    break;
+
+                case "empty":
+                    set.clear();
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
