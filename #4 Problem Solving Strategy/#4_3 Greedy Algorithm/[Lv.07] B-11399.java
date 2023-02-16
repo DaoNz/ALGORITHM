ad61262cc1280455afb87d36f646ce8a1dc3ffb9
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-16 23:30:02 +0900

    [BaekJoon] [S4] ATM
    사람의 수, 각각의 사람이 돈을 뽑는데 몇분이나 걸리는지가 주어진다.
    이 사람들을 어떻게 배치해야 각 사람이 돈을 뽑는 시간의 합이 최솟값이 되는지 구하는 문제.
    
    돈을 뽑는데 걸리는 시간이 제일 작은 순서대로 정렬하여 풀이.

---
+++ #4 Problem Solving Strategy/#4_3 Greedy Algorithm/[Lv.07] B-11399.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(rd.readLine());
+        int[] data = new int[N];
+        int res = 0;
+
+        StringTokenizer token = new StringTokenizer(rd.readLine(), " ");
+        for (int i = 0; i < N; i++)
+            data[i] = Integer.parseInt(token.nextToken());
+
+        Arrays.sort(data);
+
+        for (int i = 0; i < N; i++) {
+            res += (data[i]*(N-i));
+        }
+        System.out.print(res);
+    }
+}
