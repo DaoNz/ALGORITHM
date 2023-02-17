5ea066de0d0c2728f98738f0e481c8c368f15cd9
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-18 08:47:39 +0900

    [BaekJoon] [S3] 회전하는 큐
    큐의 크기 N과 큐에서 뽑아내려고 하는 수의 개수 M이 주어지고,
    두번째 줄에 뽑아내려고 하는 수가 차례대로 주어진다.
    
    도넛 모양의 큐를 만들어놓고 큐를 회전시키며 한곳에서 원소를 뽑아내는데,
    뽑아내려는 수를 차례대로 뽑을 때 큐를 몇번 회전시켜야 하는지 최소값을 출력하는 문제이다.
    
    자료구조의 앞뒤에서 값을 입출력할 때 시간이 적게 소요되고, IndexOf 메소드를 사용할 수 있는 LinkedList로 구현했다.
    
    뽑아내려는 수를 맨 앞으로 가져와서 뽑도록 구현했다.
    가져오려는 수의 인덱스값이 리스트의 사이즈/2 보다 크다면 리스트의 뒤쪽에 있는 요소를 앞으로 하나씩 가져오도록 했고,
    리스트의 사이즈/2 보다 작거나 같다면 앞쪽에 있는 요소들을 뒤로 하나씩 치워주도록 했다.
    
    리스트의 사이즈가 10(짝수)일 경우 사이즈/2는 5가 되고, 인덱스 5를 가진 요소가 인덱스 0이 되려면
    5>4>3>2>1>0 // 5>6>7>8>9>0 이므로 어느 방향으로 가도 상관 없지만,
    리스트의 사이즈가 11(홀수)일 경우 사이즈/2는 5가 되고, 똑같이 5를 가진 요소가 0이 되려면
    5>4>3>2>1>0 // 5>6>7>8>9>10>0 이므로 앞으로 한칸씩 이동하는게 더 빠르다.
    
    이 짝수와 홀수의 연산 차이 때문에 사이즈/2보다 인덱스가 작거나 같으면 앞으로 이동하게 해주어야 식이 성립한다.

---
+++ #9 Data Structures/#9_3 Deque/[Lv.08] B-1021.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        LinkedList<Integer> deq = new LinkedList<>();
+        StringTokenizer token = new StringTokenizer(rd.readLine(), " ");
+        int N = Integer.parseInt(token.nextToken());
+        int M = Integer.parseInt(token.nextToken());
+        int res = 0;
+
+        for (int i = 1; i <= N; i++) deq.offer(i);
+
+        token = new StringTokenizer(rd.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(token.nextToken());
+            int size = deq.size();
+            int index = deq.indexOf(target);
+
+            if (index > size / 2) {
+                while (!(deq.getFirst() == target)) {
+                    deq.offerFirst(deq.pollLast());
+                    res++;
+                }
+            } else {
+                while (!(deq.getFirst() == target)) {
+                    deq.offerLast(deq.pollFirst());
+                    res++;
+                }
+            }
+            deq.pollFirst();
+        }
+
+        System.out.println(res);
+    }
+}
