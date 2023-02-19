b2c081805091d56fc5d2400ed4652f933f977b58
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-02-20 07:24:47 +0900

    [BaekJoon] [S4] 카드 2
    정수 N이 주어진다.
    카드 덱에는 1부터 N까지의 숫자를 가진 카드가 차례대로 들어있다.
    
    제일 위에 있는 카드를 버리고, 다시 제일 위에 있는 카드를 덱의 맨 아래에 둔다.
    이 행동을 카드가 한장 남을 때까지 반복하고, 마지막에 남은 카드의 숫자가 몇인지 출력하는 문제.
    
    Queue로 간단하게 구현 후 마지막 카드를 poll 하며 출력하는 방식으로 풀이 했다.

---
+++ #9 Data Structures/#9_2 Queue/[Lv.07] B-2164.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Queue<Integer> deq = new LinkedList<>();
+        for (int i = 1; i <= N; i++) deq.offer(i);
+
+        while (deq.size() != 1) {
+            deq.poll();
+            deq.offer(deq.poll());
+        }
+        System.out.print(deq.poll());
+    }
+}
