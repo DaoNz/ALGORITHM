976d27d39cacaf2bfa1b2220c0bb9dcbf4ae15e9
DaoN <115969077+DaoNz@users.noreply.github.com>
2023-04-23 22:12:11 +0900

    [Baekjoon] [B5] 개수 세기

---
+++ #99_Other/#991_Implementation/LV01_10807.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        
+        for(int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+         }
+        
+        int M = sc.nextInt();
+        for(int e : arr) {
+            if (e == M) count++;
+        }
+        System.out.println(count);
+    }
+}
