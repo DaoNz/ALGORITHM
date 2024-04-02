52dc03f6e9b90425e6043acf7e272057cbdff29b
DaoNz <ionmax5602@naver.com>
2024-04-02 12:32:40 +0900

    Init Main

--- Main.java
+++ Main.java
@@ -1,25 +1,6 @@
-import java.util.*;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		Stack<Integer> st = new Stack<>();
-		Queue<Integer> q = new LinkedList<>();
-		int N = sc.nextInt();
-		for (int i = 0; i < N; i++) q.offer(sc.nextInt());
-
-		for (int i = 1; i <= N; i++) {
-			st.push(i);
-			sb.append("+\n");
-			while (!st.isEmpty() && !q.isEmpty() && Objects.equals(st.peek(), q.peek())) {
-				st.pop();
-				q.poll();
-				sb.append("-\n");
-			}
-		}
-		if (!st.isEmpty() || !q.isEmpty()) System.out.println("NO");
-		else System.out.println(sb.toString());
+		System.out.println("HELLO WORLD");
 	}
 }
